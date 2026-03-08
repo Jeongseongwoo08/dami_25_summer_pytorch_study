6e1b59704c613d7eec8c78390b3da899da03cf60
Jeongseongwoo08 <tnt831@kangwon.ac.kr>
2026-03-08 11:50:11 +0000

    sft_trainer

--- finetuning_qwen/SFT_Trainer/conf/config.py
+++ finetuning_qwen/SFT_Trainer/conf/config.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 from hydra.core.config_store import ConfigStore
 
@@ -6,25 +6,31 @@ from hydra.core.config_store import ConfigStore
 class BaseConfig:
     model_name: str = "Qwen/Qwen3-1.7B"
     dataset_name: str = "coastral/korean-writing-style-instruct"
-    epoch_count : int = 3
-    lr: float = 0.0001
-    batch_size: int = 4
     max_length: int = 1024
     seed: int = 42
-    weight_decay: float = 0.01
 
 @dataclass
-class LoRAConfig(BaseConfig):
+class LoRAParams:
     lora_r: int = 16
     lora_alpha: int = 32
     lora_dropout: float = 0.05
 
+@dataclass
+class SFTParams:
+    lr: float = 0.0001
+    epoch_count: int = 3
+    batch_size: int = 4
+    weight_decay: float = 0.01
+
+# 메인 설정에서 세 가지를 하나로 묶음
+@dataclass
+class MainConfig:
+    base: BaseConfig = field(default_factory=BaseConfig)
+    lora: LoRAParams = field(default_factory=LoRAParams)
+    sft: SFTParams = field(default_factory=SFTParams)
+
 cs = ConfigStore.instance()
 cs.store(
-    name="coastral_config", 
-    node=BaseConfig
-)
-cs.store(
-    name="lora_config",
-    node=LoRAConfig
+    name="qwen_sft_config",
+    node=MainConfig
 )
